b69e85a1619bea6af3a5d8e1d82f8d51fbe00f88
Virgile DESVAUX <videsvau@e1r3p11.42.fr>
2018-04-12 21:27:47 +0200

    removed ft_putchar

--- src/parsing/tokenize_splitted.c
+++ src/parsing/tokenize_splitted.c
@@ -6,7 +6,7 @@
 /*   By: videsvau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/27 16:33:23 by videsvau          #+#    #+#             */
-/*   Updated: 2018/04/11 17:06:07 by videsvau         ###   ########.fr       */
+/*   Updated: 2018/04/12 21:27:25 by videsvau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,6 @@ char		*add_aggr_file(int redir_type[3], t_inpl **inpl)
 	char	*file;
 
 	file = get_file_name(&(*inpl)->next->inp);
-	ft_putchar((*inpl)->inp->c);
 	redir_type[0] = (*inpl)->inp->c - 48;
 	redir_type[1] = TOFILE;
 	redir_type[2] = -1;
